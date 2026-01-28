2bc914a47ae4a6bc88407e02d1009a6cb61e43b6
박 영택 <youngtack.park@incross.local>
2026-01-29 08:58:18 +0900

    feat: remove GitHub link from footer

--- components/blog-footer.js
+++ components/blog-footer.js
@@ -3,9 +3,7 @@ class BlogFooter extends HTMLElement {
     this.innerHTML = `
       <footer class="neo-card" style="margin-top: 3rem; text-align: center; background-color: var(--border-color); color: var(--bg-color);">
         <p style="color: inherit;">&copy; 2026 Incross AI Biz Solution Team. All rights reserved.</p>
-        <div style="margin-top: 1rem;">
-          <a href="https://github.com/aibizsolution/blog" class="neo-btn" style="background-color: var(--accent-color); color: var(--border-color); text-decoration: none; font-size: 0.8rem;">GitHub Repo</a>
-        </div>
+
       </footer>
     `;
   }
